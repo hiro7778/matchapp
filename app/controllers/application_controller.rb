f3723b7119ade784ffbbf151c58faa9b37a5ab4a
hiro7778 <stfhm10969@gmail.com>
2020-10-15 15:18:34 +0900

    カラム名の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action  :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender, :age, :area_id, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :area_id, :birth_date, gender:[] ])
   end
 end
